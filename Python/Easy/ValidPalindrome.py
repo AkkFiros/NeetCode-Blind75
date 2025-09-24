faa6568602457b2c7468d4dbbdec8387124b780d
AkkFiros <khantkyaw100@gmail.com>
2025-09-24 20:14:43 +0800

    Update ValidPalindrome.py with full solution

--- Python/Easy/ValidPalindrome.py
+++ Python/Easy/ValidPalindrome.py
@@ -8,6 +8,7 @@ Task:
 Given astring s, return true if s is a palindrome, and false otherwise.
 A palindrome is a string that reads the same forward and backward.
 It is also case-insensitive and ignores all non-alphanumeric characters.
+Note: s is length between 1 and 1000, inclusive.
 """
 
 class Solution:
@@ -25,3 +26,24 @@ class Solution:
         # strip s of all non-alphanumeric characters, convert all to lowercase
         filtered_s = "".join(char.lower() for char in s if char.isalnum())
 
+        len_s = len(filtered_s)
+
+        # edge case: if s has only 1 chaaracter
+        if len_s == 1:
+            return True
+        
+        # initialise pointers
+        pointer1 = 0
+        pointer2 = len_s - 1
+
+        while pointer2 > pointer1:
+            char1 = filtered_s[pointer1]
+            char2 = filtered_s[pointer2]
+
+            if char1 != char2:
+                return False
+            
+            pointer1 += 1
+            pointer2 -= 1
+
+        return True
